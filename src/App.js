ed3e0aa7e4e4f903ea250a015482b8db9fec0d2b
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-08 00:30:50 -0400

    importing our components in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// we are importing all the compoent and our css 
 import './App.css';
 import Sidebar from "./components/sidebar";
 import Reviews from "./components/reviews";
@@ -5,16 +6,7 @@ import Rating from "./components/average-rating";
 import Sentiment from "./components/sentiment-analysis";
 import Visitors from "./components/web-visitors";
 
-function App() {
-  return (
-    <div className="dashboard">
-      <Sidebar/>
-      <Reviews/>
-      <Rating/>
-      <Sentiment/>
-      <Visitors/>
-    </div>
-  );
-};
 
+
+// this will set our page to be able to imort in different pages components and app.js 
 export default App;
