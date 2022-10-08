2c8d7b38e426be22edf8cfae9b3d368c122b239e
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-08 00:31:23 -0400

    calling our components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ import Rating from "./components/average-rating";
 import Sentiment from "./components/sentiment-analysis";
 import Visitors from "./components/web-visitors";
 
-
+// we are calling for our components in our app
+function App() {
+  return (
+    <div className="dashboard">
+      <Sidebar/>
+      <Reviews/>
+      <Rating/>
+      <Sentiment/>
+      <Visitors/>
+    </div>
+  );
+};
 
 // this will set our page to be able to imort in different pages components and app.js 
 export default App;
