0448130209635d99a281b245f49c2ef48eee18cd
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-08 00:18:22 -0400

    Creating Average Ratin compo

--- src/components/average-rating.js
+++ src/components/average-rating.js
@@ -1,10 +1,14 @@
+//This is the rating component 
 function Rating(){
-    return(
-        <div className="rating">
-            <h4>Average Rating</h4>
-            <h2>4.6</h2>
-        </div>
-    );
+
+// This will return a div with class name "rating"
+// Inside the div we wanna add the "Average Rating"
+return(
+    <div className="rating">
+        <h4>Average Rating</h4>
+        <h2>4.6</h2>
+    </div>
+);
 };
 
 export default Rating;
